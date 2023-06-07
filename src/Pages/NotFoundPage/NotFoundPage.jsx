809a0ed50ad20000c62acc3e56489e16e5463fcb
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-06-07 19:24:34 +0600

    add back to home button on 404 page

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,18 @@
 import Lottie from "lottie-react";
 import error404 from "../../assets/404Page/error404.json";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
     return (
         // lottie animation
-        <div className="max-w-7xl mx-auto h-screen">
-            <Lottie className="h-full" animationData={error404} loop={true} />
+        <div className="max-w-7xl mx-auto h-screen p-10">
+            <div className="h-[500px]">
+                <Lottie className="h-full" animationData={error404} loop={true} />
+            </div>
+            <div className="text-center mt-4">
+                <Link to="/" className="btn btn-primary">Home</Link>
+            </div>
+
         </div>
     );
 };
