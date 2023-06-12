0bcf6ecc653fcd2f68d069ceb9f6a7b157a19463
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-06-12 19:24:40 +0600

    add register page with react hook form with basic setup

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,30 @@
+import { useForm } from "react-hook-form";
 
 
 const Register = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
         <div>
-            this is register
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {/* Name */}
+                <div>
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" {...register("name")} placeholder="Your Name" className="input input-bordered" />
+                </div>
+                {/* Email */}
+                <div>
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="email" {...register("email")} placeholder="Your Email" className="input input-bordered" />
+                </div>
+
+            </form>
         </div>
     );
 };
