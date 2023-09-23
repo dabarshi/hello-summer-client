9aadaaa637a43c2794ed204596891a157d6fd964
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-09-24 01:45:07 +0600

    changed navbar background color

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -7,11 +7,11 @@ import Container from "../components/Container/Container";
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Container>
-                <Navbar></Navbar>
                 <Outlet></Outlet>
-                <Footer></Footer>
             </Container>
+            <Footer></Footer>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,36 +11,36 @@ const Navbar = () => {
     const navItem = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/classes">Classes</Link></li>
-        { user && <li><Link to="/dashboard/user-home">Dashboard</Link></li>}
+        {user && <li><Link to="/dashboard/user-home">Dashboard</Link></li>}
     </>;
 
     // handle logout 
     const handleLogout = () => {
         logOut()
-            .then(() => { 
+            .then(() => {
                 navigate('/')
             })
             .catch(error => console.log(error))
     }
     return (
-        <div className="navbar bg-neutral text-neutral-content rounded-lg mb-10">
+        <div className="navbar max-w-7xl mx-auto mb-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-52">
                         {
                             navItem
                         }
                     </ul>
                 </div>
                 <Link to="/" className="btn btn-ghost normal-case text-xl">
-                    <img  className='none md:flex md:w-20 ' src={logo} alt="Hello Summer" />
+                    <img className='none md:flex md:w-20 ' src={logo} alt="Hello Summer" />
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 font-semibold text-lg">
                     {navItem}
                 </ul>
             </div>
@@ -54,12 +54,12 @@ const Navbar = () => {
                                         <img title={user?.displayName} src={user?.photoURL} />
                                     </div>
                                 </div>
-                                <button onClick={handleLogout} className="btn btn-primary text-white bg-[#005BA2] hover:bg-[#07416d]">Logout</button>
+                                <button onClick={handleLogout} className="btn text-white bg-[#ec7d4a] hover:bg-[#fb8b57]">Logout</button>
                             </div>
                         </> :
                         <>
                             <Link to="/login">
-                                <button className="btn btn-primary text-white bg-[#005BA2] hover:bg-[#07416d]">Login</button>
+                                <button className="btn text-white bg-[#ec7d4a] hover:bg-[#fb8b57]">Login</button>
                             </Link>
                         </>}
             </div>
